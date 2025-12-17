4ebc78289abfd80570676067103ea6040a3c1975
ITANITI PENGJAEM <6810110432@psu.ac.th>
2025-12-17 10:21:18 +0700

    P22

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookCategoryModule } from './book-category/book-category.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'bookstore_dev',
+      entities: [], // เราจะเพิ่ม Entities ที่นี่ในภายหลัง
+      synchronize: true, // สร้าง Table อัตโนมัติ (ใช้สำหรับ Dev เท่านั้น)
+    }),
+    BookCategoryModule,
+  ],
+})
+export class AppModule {}
